9bf7f748f1d9c31b733e61818f5fccdef1d60d00
yusrilarzaqi <arzaqiyusril@gmail.com>
2021-12-30 23:26:41 +0700

    deleted:    ../config/fish/conf.d/omf.fish 	modified:   ../config/fish/config.fish 	modified:   ../config/fish/fish_variables 	modified:   ../config/i3/config 	modified:   ../config/kitty/font.conf 	modified:   ../config/kitty/kitty.conf 	deleted:    ../config/polybar/a.sh 	deleted:    ../config/polybar/battery.sh 	modified:   ../config/polybar/config 	deleted:    ../config/polybar/date.sh 	deleted:    ../config/polybar/ram.sh 	deleted:    ../config/polybar/script.sh 	deleted:    ../config/polybar/utils.sh 	modified:   ../tmux.conf 	modified:   vimrc

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,14 +3,10 @@ if status is-interactive
     #set BLUE "\e[94m"
     #set BLUE "\e[97m"
     #set STOP "\e[0m"
-    # set time (date +"%A, %d %m %y %H : %M%p : %S")
     
     function fish_greeting
-        #printf "$BLUE"
         set_color brblue
-        # figlet -f slant "$time"
         figlet -f slant "Yusril Arzaqi"
-        # fish_logo brblue brmagenta brgreen
         #printf "$STOP"
     end
 end
@@ -21,26 +17,19 @@ alias grep='grep --color=auto --exclude-dir={.bzr,.git, CVS, .hg, .svn, .idea, .
 alias v='vim'
 alias ls='exa --icons'
 alias lsa='ls -a'
+alias graph='git log --oneline --all --graph --decorate'
 
 
-function fish_user_key_bindings
-    fish_vi_key_bindings
-    #fish_default_key_bindings
-end
+#function fish_user_key_bindings
+#    fish_vi_key_bindings
+#    #fish_default_key_bindings
+#end
 export TERM="xterm-256color"
 
-
-## Configure Agnoster
-# set -g color_dir_bg green
-# set -g color_vi_mode_insert green
-# set -g color_vi_mode_normal blue
-# set -g color_git_dirty_bg red
-# set -g color_git_bg blue
-# set -g color_vi_mode_visual purple
-
+starship init fish | source
 # Add path
-set -U fish_user_paths /home/yusril/.local/bin $fish_user_paths
-set -U fish_user_paths /home/yusril/bin $fish_user_paths
+# set -U fish_user_paths /home/yusril/.local/bin $fish_user_paths
+# set -U fish_user_paths /home/yusril/bin $fish_user_paths
 
 
-
+starship init fish | source
