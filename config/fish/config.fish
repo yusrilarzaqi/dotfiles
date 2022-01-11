32b162ed0fce6714fed437257a1ecc23fe0b8918
yusrilarzaqi <arzaqiyusril@gmail.com>
2022-01-11 14:50:08 +0700

    update my dotfiles

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,23 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    #set BLUE "\e[94m"
-    #set BLUE "\e[97m"
-    #set STOP "\e[0m"
+    set STOP "\e[0m"
+    set orange "#fe8019"
+    set blue "#7AA2F7"
     
     function fish_greeting
-        set_color brblue
-        figlet -f slant "Yusril Arzaqi"
-        #printf "$STOP"
+        set_color $orange
+        echo "
+          ██░ ██  ▓█████  ██▓     ██▓    ▒█████      ▓██   ██▓ █    ██   ██████  ██▀███    ██▓  ██▓        ▄▄▄      ██▀███  ▒███████▒ ▄▄▄       █████    ██▓
+        ▒▓██░ ██  ▓█   ▀ ▓██▒    ▓██▒   ▒██▒  ██▒     ▒██  ██▒ ██  ▓██▒▒██    ▒ ▓██ ▒ ██▒▒▓██▒ ▓██▒       ▒████▄   ▓██ ▒ ██▒▒ ▒ ▒ ▄▀░▒████▄   ▒██▓  ██▒▒▓██▒
+        ░▒██▀▀██  ▒███   ▒██░    ▒██░   ▒██░  ██▒      ▒██ ██░▓██  ▒██░░ ▓██▄   ▓██ ░▄█ ▒▒▒██▒ ▒██░       ▒██  ▀█▄ ▓██ ░▄█ ▒░ ▒ ▄▀▒░ ▒██  ▀█▄ ▒██▒  ██░▒▒██▒
+         ░▓█ ░██  ▒▓█  ▄ ▒██░    ▒██░   ▒██   ██░      ░ ▐██▓░▓▓█  ░██░  ▒   ██▒▒██▀▀█▄  ░░██░ ▒██░       ░██▄▄▄▄██▒██▀▀█▄    ▄▀▒   ░░██▄▄▄▄██░██  █▀ ░░░██░
+         ░▓█▒░██▓▒░▒████▒░██████▒░██████░ ████▓▒░      ░ ██▒▓░▒▒█████▓ ▒██████▒▒░██▓ ▒██▒░░██░▒░██████     ▓█   ▓██░██▓ ▒██▒▒███████▒▒▓█   ▓██░▒███▒█▄ ░░██░
+          ▒ ░░▒░▒░░░ ▒░ ░░ ▒░▓  ░░ ▒░▓  ░ ▒░▒░▒░        ██▒▒▒ ░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░░ ▒▓ ░▒▓░ ░▓  ░░ ▒░▓       ▒▒   ▓▒█░ ▒▓ ░▒▓░░▒▒ ▓░▒░▒░▒▒   ▓▒█░░ ▒▒░ ▒  ░▓
+          ▒ ░▒░ ░░ ░ ░  ░░ ░ ▒  ░░ ░ ▒    ░ ▒ ▒░      ▓██ ░▒░ ░░▒░ ░ ░ ░ ░▒  ░ ░  ░▒ ░ ▒ ░ ▒ ░░░ ░ ▒        ░   ▒▒   ░▒ ░ ▒  ░▒ ▒ ░ ▒░ ░   ▒▒  ░ ▒░  ░ ░ ▒ ░
+          ░  ░░ ░    ░     ░ ░     ░ ░  ░ ░ ░ ▒       ▒ ▒ ░░   ░░░ ░ ░ ░  ░  ░    ░░   ░ ░ ▒ ░   ░ ░        ░   ▒    ░░   ░ ░ ░ ░ ░ ░  ░   ▒     ░   ░ ░ ▒ ░
+          ░  ░  ░░   ░  ░    ░  ░    ░      ░ ░       ░ ░        ░           ░     ░       ░  ░    ░            ░     ░       ░ ░          ░      ░      ░
+        "
+        # figlet -f script -p "Hello Yusril Arzaqi"
     end
 end
 
@@ -18,6 +28,7 @@ alias v='vim'
 alias ls='exa --icons'
 alias lsa='ls -a'
 alias graph='git log --oneline --all --graph --decorate'
+alias ip='ip -c'
 
 
 #function fish_user_key_bindings
@@ -30,6 +41,5 @@ starship init fish | source
 # Add path
 # set -U fish_user_paths /home/yusril/.local/bin $fish_user_paths
 # set -U fish_user_paths /home/yusril/bin $fish_user_paths
-
 
 starship init fish | source
